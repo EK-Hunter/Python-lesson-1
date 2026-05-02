3b00305e1309eb26b57f434314c7f8ba9a96bb34
EK-Hunter <oluwakoredea12@gmail.com>
2026-05-02 13:24:45 +0100

    my programme to explore variables dictionaries etc

---
+++ INTRO.py
@@ -0,0 +1,27 @@
+print("hi world")
+enoch = "hi"
+print(enoch)
+name = "enoch"
+print(name)
+names = ["enoch" , "john" , "Tanshi"]
+print(names)
+
+
+#dictionaries are used to store values in pairs
+#these pairs are called key value pairs
+dictionary = {"france":"paris" , 
+              "tanshi":30,
+              "john":76}
+
+print(dictionary)
+print(dictionary.keys())
+print(dictionary.values())
+
+dict2 = {"france":"paris",
+       "uk":"london",
+       "italy":"rome",
+       "spain":"madrid",
+       "nigeria":"abuja"}
+print(dict2)
+print(dict2.keys())
+print(dict2.values())
